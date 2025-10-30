929d789702bf7cf5eb6ec812e898183011db0276
Many0nne <ptitesaile@gmail.com>
2025-10-30 15:57:33 +0100

    first commit

---
+++ src/components/Map.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
+const Map: React.FC = () => {
+  return (
+    <MapContainer center={[47.2184, -1.5536]} zoom={12} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap"
+      />
+      <Marker position={[47.2184, -1.5536]}>
+        <Popup>Centre de Nantes</Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
+
+export default Map
